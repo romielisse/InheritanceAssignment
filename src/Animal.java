2cf252bf3046440d09ff71d499e3290fe4b5a75d
Melissa Fung <romielisse@gmail.com>
2018-06-13 09:35:10 -0400

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,8 @@
+public class Animal {
+
+    public Animal(){}
+
+    public void eat(){}
+
+    public void sleep(){}
+}
---
+++ src/Bird.java
@@ -0,0 +1,5 @@
+public class Bird extends Animal {
+    public Bird(){}
+
+    public void fly(){}
+}
